4a33707c59125a7288bae3204cdf7dfd990868b3
JerryZzhu <1304105887@qq.com>
2022-04-06 17:47:33 +0800

    create project

---
+++ src/main/java/com/JerryZzhu/foranimal/ForanimalApplication.java
@@ -0,0 +1,13 @@
+package com.JerryZzhu.foranimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForanimalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForanimalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/JerryZzhu/foranimal/ForanimalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JerryZzhu.foranimal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForanimalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
